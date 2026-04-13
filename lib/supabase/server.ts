d0b08754eca880c18a79bece84f7e9092c7483ea
Gjsb-Sisprot <gsanchez@sisprotgf.com>
2026-04-13 02:03:23 -0400

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,8 +6,8 @@ export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL?.trim() || "",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.trim() || "",
+    "https://mkluqieffbwelhkxbovk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rbHVxaWVmZmJ3ZWxoa3hib3ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4MDI1NjMsImV4cCI6MjA4MzM3ODU2M30.Mb_H5skSS6QcUz5vKi23AG7PZDamjwUia7fLugtnu_8",
     {
       cookies: {
         getAll() {
