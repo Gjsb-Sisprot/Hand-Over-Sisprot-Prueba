c7f43e309f258dbd6f365bbd64d08830ca3cc7df
Gjsb-Sisprot <gsanchez@sisprotgf.com>
2026-04-13 02:03:37 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,7 @@ import { Database } from "@/types/database";
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL?.trim() || "",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.trim() || ""
+    "https://mkluqieffbwelhkxbovk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rbHVxaWVmZmJ3ZWxoa3hib3ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4MDI1NjMsImV4cCI6MjA4MzM3ODU2M30.Mb_H5skSS6QcUz5vKi23AG7PZDamjwUia7fLugtnu_8"
   );
 }
