73da63895bfc4db06c0ec7f073892f61ac29e11d
Gjsb-Sisprot <gsanchez@sisprotgf.com>
2026-04-21 03:24:04 -0400

    Update index.ts

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
-export { useRealtimeConversations } from "./use-realtime-conversations";
+export { useDashboardConversations } from "./use-realtime-conversations";
+export { useDashboardMessages } from "./use-realtime-messages";
 export { useRealtimeStats } from "./use-realtime-stats";
 export { usePermissions } from "./use-permissions";
